3dd03a18411c12362a3fdef11d22b8d4f78e8f52
villiant <pilot.cool@mail.ru>
2021-06-17 02:35:58 +0300

    hw01 done

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
 	// Place your code here.
+	var phrase string = "Hello, OTUS!"
+
+	fmt.Println(stringutil.Reverse(phrase))
 }
